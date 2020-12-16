b984e408f84be0061f82e964291c2e11390c90e9
Kaspars Liepins <kasparslie@gmail.com>
2020-12-16 16:50:12 +0100

    innitial push

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+require('dotenv').config();
+const connection = require('./connection');
+
+app.get('/', (_, res) => res.json({ message: 'Hello World!' }))
+
+  
+  app.post("/bookmarks", (req, res) => {
+    const { url, title } = req.body;
+    connection.query(
+      "INSERT INTO bookmark (url, title) VALUES(?, ?)",
+        [url, title ],
+      (err, results) => {
+            if (err) {
+              console.log(err);
+              res.status(500).send("Error saving a product");
+            } else {
+              res.status(200).json(results);
+            }
+        }
+     );
+});
+
+app.get("//bookmarks/:id", (req, res) => {
+        const myCat= req.params.category
+        console.log(req.params.category)
+        connection.query(
+          "SELECT * from product where category=?", [myCat], 
+          (err, results) => {
+            if (err) {  
+              console.log(err);
+              res.status(500).send("Error retrieving data");
+            } else {
+              res.status(200).json(results);
+            }
+          }
+        );
+      });
+  
+
+
+
 
 module.exports = app;
---
+++ test/app.integration.spec.js
@@ -0,0 +1,52 @@
+// test/app.integration.spec.js
+const { text } = require('body-parser');
+const request = require('supertest');
+const app = require('../app');
+
+
+describe('Test routes', () => {
+    it('GET / sends "Hello World" as json', (done) => {
+      request(app)
+        .get('/')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then(response => {
+          const expected = { message: 'Hello World!'};
+          expect(response.body).toEqual(expected);
+          done();
+        });
+    });
+  })
+
+  describe('Test routes', () => {
+  it('POST /bookmarks - error (fields missing) ', (done) => {
+    request(app)
+    .post('/bookmarks')
+    .send({})
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .then(response => {
+        const expected = { id: expect.any(Number), url: expect(Text), title: expect(Text) };
+      done();
+    });
+});
+});
+
+describe('GET /bookmarks/:id', () => {
+    it('GET /bookmarks/:id - error (fields missing) ', (done) => {
+    const testBookmark = { url: 'https://nodejs.org/', title: 'Node.js' };
+    beforeEach((done) => connection.query(
+      'TRUNCATE bookmark', () => connection.query(
+        'INSERT INTO bookmark SET ?', testBookmark, done
+      )
+    ));
+    request(app)
+    .get('/bookmarks')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .then(response => {
+      const expected = { error: 'Bookmark not found' }
+      done();
+    });
+});
+});
